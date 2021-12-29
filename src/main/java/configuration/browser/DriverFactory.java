bb887a9de38ba2d6b62da76b7b6a5a5005bc8be1
KasiaGP <kasiagrochal@hotmail.com>
2021-12-29 15:48:19 +0100

    remove weblistener

--- src/main/java/configuration/browser/DriverFactory.java
+++ src/main/java/configuration/browser/DriverFactory.java
@@ -10,13 +10,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
-import configuration.WebListener;
 
 public class DriverFactory {
-
-    private EventFiringWebDriver webDriver;
-    private WebListener webListener;
 
     public WebDriver getDriver(Browser browser) {
         switch (browser) {
